0f92968f89fc3e9bb6f6c47ca38e2659ecfcc817
imarinfr <ivan.marin.franch@gmail.com>
2018-03-25 12:12:59 -0500

    change OCT angle reference to 9-o'clock position (right eye) in psi2oct as in G-H 2000

--- source/R/psi2oct.R
+++ source/R/psi2oct.R
@@ -6,6 +6,7 @@ psi2oct <- function( psi, diam = 12, xy0 = c( 15, 2 ) ) {
     fb <- fiberpathpsi( psi[i] )
     xy <- jpolar2cart( data.frame( r = r, phi = fb( r ) ) ) - xy0
     xy <- xy[which.min( abs( xy$x^2 + xy$y^2 - radius^2 ) ),]
+    xy <- -xy
     oct2 <- atan( abs( xy[,2] / xy[,1] ) )
     
     oct2[is.nan( oct2 )]         <- 0
