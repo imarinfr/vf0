cb283784e705a77a0edab32f45bd2a9725404dea
imarinfr <ivan.marin.franch@gmail.com>
2018-04-01 10:34:21 -0500

    reworking the Jansonius map

--- source/R/cart2jpolar.R
+++ source/R/cart2jpolar.R
@@ -1,21 +1,11 @@
-cart2jpolar <- function( xy, xy0 = c( 15, 2 ) ) {
+cart2jpolar <- function( xy ) {
+  x  <- xy[,1]
+  y  <- xy[,2]
+  xp <- x - 15
+  yp <- y
+  yp[x > 0] <- y[x > 0] - 2 * ( x[x > 0] / 15 )^2
 
-  idx <- which( xy[,1] > 0 )
-  xy[idx,2] = xy[idx,2] - xy0[2] * ( xy[idx,1] / xy0[1] )^2
-  xy[,1] <- xy[,1] - xy0[1]
-
-  r   <- sqrt( xy[,1]^2 + xy[,2]^2 )
-  psi <- atan( abs( xy[,2] / xy[,1] ) )
-
-  psi[is.nan( psi )]             <- 0
-  psi[xy[,1] > 0 & xy[,2] < 0]   <- 2 * pi - psi[xy[,1] > 0 & xy[,2] < 0]
-  psi[xy[,1] < 0 & xy[,2] > 0]   <- pi - psi[xy[,1] < 0 & xy[,2] > 0]
-  psi[xy[,1] < 0 & xy[,2] < 0]   <- psi[xy[,1] < 0 & xy[,2] < 0] + pi
-  psi[xy[,1] >= 0 & xy[,2] == 0] <- 0
-  psi[xy[,1] == 0 & xy[,2] > 0]  <- pi / 2
-  psi[xy[,1] == 0 & xy[,2] < 0]  <- -pi / 2
-  psi[xy[,1] < 0 & xy[,2] == 0]  <- pi
-  psi[psi < 0]                   <- 2 * pi + psi[psi < 0]
-
+  r   <- sqrt( xp^2 + yp^2 )
+  psi <- atan2( yp, xp )
   return( data.frame( r = r, psi = 180 / pi * psi ) )
 }
--- source/R/gcloc2psi.R
+++ source/R/gcloc2psi.R
@@ -1,20 +1,19 @@
-gcloc2psi <- function( xy, xy0 = c( 15, 2 ), r0 = 4 ) {
+gcloc2psi <- function( xy, r0 = 4 ) {
   # Returns the angle of incidence at the ONH of a vf locations
-  psi0 <- rep( NA, nrow( xy ) )
-  rpsi <- cart2jpolar( xy, xy0 )
+  xy[,2] <- -xy[,2]
+  psi0   <- rep( NA, nrow( xy ) )
+  rpsi   <- cart2jpolar( xy )
   for( i in 1:nrow( xy ) ) {
     fbpathinv <- function( psi ) {
-      if( psi < 0 ) psi <- psi + 360
       fb <- fiberpathpsi( psi, r0 )
       psiest <- fb( rpsi[i,1] )
       return( ( psiest - rpsi[i,2] )^2 )
     }
     psi00 <- c( 60, 180 )
     if( xy[i,2] < 0 ) psi00 <- c( -180, -60 )
-    if( xy[i,1] > xy0[1] ) psi00 <- c( -60, 60 )
+    if( xy[i,1] > 15 ) psi00 <- c( -60, 60 )
     
     psi0[i] <- optimize( fbpathinv, interval = psi00 )$minimum
-    if( psi0[i] < 0 ) psi0[i] <- psi0[i] + 360
   }
   return( psi0 )
 }
