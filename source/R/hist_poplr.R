03052242ec1f1d8c0730a3552479d3e09f0fa0cd
imarinfr <ivan.marin.franch@gmail.com>
2018-05-30 19:28:33 -0500

    improved coding and visualization of hist_poplr. From now on, S / n where n is the number of locations is presented in the permutation histogram, so that we can keep the same x range for all types of visual field patterns of locations.

--- source/R/hist_poplr.R
+++ source/R/hist_poplr.R
@@ -1,25 +1,31 @@
-hist_poplr <- function( scomb_obs, pcomb_obs, scomb, txtfont = "sans", pointsize = 10 ) {
+hist_poplr <- function( scomb_obs, pcomb_obs, scomb, nLoc = 52, txtfont = "sans", pointsize = 10 ) {
 
-  limExtention <- 2
-  sizeBreaks   <- 4
-  breaks       <- c( 0:ceil( max( scomb[scomb != Inf] ) / sizeBreaks ) * sizeBreaks )
-  hcomb <- hist( scomb, breaks = breaks, plot = FALSE )
-  hcomb$counts <- hcomb$counts / max( hcomb$counts )
+  sep <- 0.1
+  scomb_obs <- scomb_obs / nLoc
+  scomb     <- scomb / nLoc
+  pcomb_txt <- paste0( "(p = ", round( pcomb_obs, 3 ), ")" )
+  if( scomb_obs <= 6 ) {
+    scomb_txt <- paste( "S / n = ", round( scomb_obs, 1 ) )
+  } else {
+    scomb_obs <- 6
+    scomb_txt <- paste( "S / n > 6" )
+  }
+  adj <- 0.5
+  if( scomb_obs < 1 ) adj <- 0
+  if( scomb_obs > 5 ) adj <- 1
+  breaks    <- seq( 0, 6, by = sep )
   ops     <- par()$ps
   ofamily <- par()$family
   oplt    <- par()$plt
   par( ps     = pointsize )
   par( family = txtfont )
   par( plt    = c( 0, 1, 0.3, 1 ) )
-  plot( hcomb, main = "", xlab = NULL, ylab = NULL, xlim = c(0, limExtention * max( scomb[scomb != Inf] ) ), ylim = c( 0, 1.1 ), border = rgb( 0.7, 0.7, 0.7 ), col = rgb( 0.9, 0.9, 0.9 ), axes = FALSE )
+  hist( scomb, breaks = breaks, freq = FALSE, main = "", xlim = c( 0, 6 ), ylim = c( 0, 1.3 ), border = rgb( 0.7, 0.7, 0.7 ), col = rgb( 0.9, 0.9, 0.9 ), axes = FALSE )
   axis( 1, las = 1, tcl = -.3, lwd = 0.5, lwd.ticks = 0.5 )
-  title( xlab = "S statistic", mgp = c( 2, 1, 0 ) )
-  lines( c( scomb_obs, scomb_obs ), c( 0, 0.60 ), col = "red" )
-  points( c( scomb_obs ), c( 0.60 ), pch = 22, col = "red", bg = "red" )
-  scomb_txt <- as.character( round( scomb_obs ) )
-  pcomb_txt <- as.character( round( pcomb_obs * 1000 ) / 1000 )
-  text( c( scomb_obs ), c( 0.90 ), labels = paste( "S =", scomb_txt ) )
-  text( c( scomb_obs ), c( 0.75 ), labels = paste( "p =", pcomb_txt ) )
+  title( xlab = "S / number of locations n", mgp = c( 2, 1, 0 ) )
+  lines( c( scomb_obs, scomb_obs ), c( 0, 1.1 ), col = "red" )
+  points( c( scomb_obs ), 1.1, pch = 21, col = "red", bg = "red" )
+  text( c( scomb_obs ), 1.2, labels = paste( scomb_txt, pcomb_txt ), adj = adj )
   par( ps     = ops )
   par( family = ofamily )
   par( plt    = oplt )
